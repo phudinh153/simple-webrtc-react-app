1c26daff09a871825a64d6051a849c3f8eeca44c
phudinh153 <95414402+phudinh153@users.noreply.github.com>
2024-02-29 21:43:19 +0700

    Update CallScreen.js

--- src/screens/CallScreen.js
+++ src/screens/CallScreen.js
@@ -10,7 +10,7 @@ function CallScreen() {
   const localVideoRef = useRef(null);
   const remoteVideoRef = useRef(null);
 
-  const socket = socketio("https://signaling-server-flask.herokuapp.com/", {
+  const socket = socketio("https://signaling-server-pfm2.onrender.com/", {
     autoConnect: false,
   });
 
@@ -25,25 +25,6 @@ function CallScreen() {
   };
 
   const startConnection = () => {
-<<<<<<< Updated upstream
-    navigator.mediaDevices
-      .getUserMedia({
-        audio: false,
-        video: {
-          height: 350,
-          width: 350,
-        },
-      })
-      .then((stream) => {
-        console.log("Local Stream found");
-        localVideoRef.current.srcObject = stream;
-        socket.connect();
-        socket.emit("join", { username: localUsername, room: roomName });
-      })
-      .catch((error) => {
-        console.error("Stream not found: ", error);
-      });
-=======
     try {
       socket.connect();
       socket.emit("join", { username: localUsername, room: roomName });
@@ -60,7 +41,6 @@ function CallScreen() {
     socket.on('error', (error) => {
       console.error("An error occurred: ", error);
     });
->>>>>>> Stashed changes
   };
 
   const onIceCandidate = (event) => {
